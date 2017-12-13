6cbfbceef1d8580ebb7124ab3ab6ee2afe06fb42
James Osborn <jamescarlosborn@gmail.com>
2017-12-12 20:50:19 -0800

    add jquery to display all results for name search.

--- js/doctor-lookup-interface.js
+++ js/doctor-lookup-interface.js
@@ -2,8 +2,10 @@ import { MedicalInfo } from "./../js/doctor-lookup.js";
 
 
 let displayName = function(result) {
-  $('#name-output').append(`The doctor's full name: ${result.name}`);
-  $('#name-output').append(result.name);
+  $('#showName').append(`The doctor's full name: ${result.data.practices.name}`);
+  $('#showAddress').append(`Address: ${result.data.practices.visit_address.street}`);
+  $('#showPhoneNumber').append(`Phone Number: ${result.data.practices.phones.number}`);
+  $('#showAvailability').append(`Accepts New Patients: ${result.data.practices.accepts_new_patients}`);
 };
 
 let displaySymptom = function(result) {
