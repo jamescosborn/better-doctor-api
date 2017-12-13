85cc5278fd48b5b21054ed27b3a6b60fbcba5df7
James Osborn <jamescarlosborn@gmail.com>
2017-12-12 21:00:15 -0800

    remove handlebars, it was not doing anything, update html for Portland.

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,12 +1,26 @@
 import { MedicalInfo } from "./../js/doctor-lookup.js";
 
+
+let displayName = function(result) {
+  $('#showName').append(`The doctor's full name: ${result.data.practices.name}`);
+  $('#showAddress').append(`Address: ${result.data.practices.visit_address.street}`);
+  $('#showPhoneNumber').append(`Phone Number: ${result.data.practices.phones.number}`);
+  $('#showAvailability').append(`Accepts New Patients: ${result.data.practices.accepts_new_patients}`);
+};
+
+let displaySymptom = function(result) {
+  $('#symptom-output').append(result.symptom);
+};
+
 $(document).ready(function() {
-  $('#name-search').click(function() {
-    let doctorName = $('#doctorName').val();
-    $('#doctorName').val("");
-  $('#symptom-search').click(function() {
-
-  })
-
+  let medicalInfo = new MedicalInfo();
+  $('#nameSearch').submit(function() {
+    let userInput = $('#doctorName').val();
+    medicalInfo.getDoctor(userInput, displayName);
   });
+
+  $('#symptomSearch').submit(function() {
+    let userInput2 = $('#symptomName').val();
+    medicalInfo.getSymptom(userInput2, displaySymptom);
+  })
 });
